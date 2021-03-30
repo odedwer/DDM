11ff35dc3b972a2211287872dcb59ebc68babaa6
Oded Wertheimer <oded.wertheimer@mail.huji.ac.il>
2021-03-30 12:09:46 +0300

    Changed preprocessing of data to include response and exclude correctness of response. Started basic plotting code

--- generate_csv_from_edat_excel.py
+++ generate_csv_from_edat_excel.py
@@ -1,26 +1,29 @@
 import pandas as pd
 import os
+import numpy as np
 
 # %%
 FINAL_FILENAME = 'PVI subjects for ddm.csv'
 FILE_PATH = r"\\ems.elsc.huji.ac.il\deouell-lab\Lab-Shared\Experiments\PVI - Priming Intentions\PVI\results"
-WANTED_COLUMNS = ['Subject', 'Trial', 'PrimeCanvas', 'TargetCanvas', 'isCorrect', 'ResponseTime']
-COLUMN_DICT = {'PrimeCanvas': 'Prime', 'TargetCanvas': 'Cue', 'ResponseTime': "RT"}
-CANVAS_DICT = {1: 'left', 2: 'right', 3: 'free choice', 4: 'neutral'}
-# load filenames
-filenames = [filename for filename in os.listdir(FILE_PATH) if
-             filename[:3].lower() == 'pvi' and filename[-4:] == 'xlsx' and 'digest' not in filename]
-# read as pandas data frames
-dataframes = [pd.read_excel(os.path.join(FILE_PATH, filename)) for filename in filenames]
-# concatenate to single dataframe and re-format
-df = pd.concat(dataframes)
-df['Trial'] = df.index + 1
-df = df[WANTED_COLUMNS]
-df.rename(columns=COLUMN_DICT, inplace=True)
-df[['Prime', 'Cue']] = df[['Prime', 'Cue']].replace(CANVAS_DICT)
-df['isCorrect'] = df['isCorrect'].astype('bool')
-df = df[df['RT'].notna()]
-df = df[df['Subject'].notna()]
-df['Subject'] = df['Subject'].astype('int')
+WANTED_COLUMNS = ['Subject', 'Trial', 'PrimeCanvas', 'TargetCanvas', 'ActualResp', 'ResponseTime']
+COLUMN_DICT = {'PrimeCanvas': 'Prime', 'TargetCanvas': 'Cue', 'ResponseTime': "RT", "ActualResp": "Resp"}
+CANVAS_DICT = {1: 'L', 2: 'R', 3: 'FC', 4: 'N'}
+RESPONSE_DICT = {"z": "L", r"{/}": "R", np.nan: "None"}
 
-df.to_csv(FINAL_FILENAME)
+if __name__ == "_main__":
+    # load filenames
+    filenames = [filename for filename in os.listdir(FILE_PATH) if
+                 filename[:3].lower() == 'pvi' and filename[-4:] == 'xlsx' and 'digest' not in filename]
+    # read as pandas data frames
+    dataframes = [pd.read_excel(os.path.join(FILE_PATH, filename)) for filename in filenames]
+    # concatenate to single dataframe and re-format
+    df = pd.concat(dataframes)
+    df['Trial'] = df.index + 1
+    df = df[WANTED_COLUMNS]
+    df.rename(columns=COLUMN_DICT, inplace=True)
+    df['Resp'].replace(RESPONSE_DICT, inplace=True)
+    df[['Prime', 'Cue']] = df[['Prime', 'Cue']].replace(CANVAS_DICT)
+    df = df[df['RT'].notna()]
+    df = df[df['Subject'].notna()]
+    df['Subject'] = df['Subject'].astype('int')
+    df.to_csv(FINAL_FILENAME)
--- main_model.py
+++ main_model.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import numpy as np
 import ddm
+import matplotlib.pyplot as plt
+import seaborn as sns
+from generate_csv_from_edat_excel import FINAL_FILENAME
 
-#%%
-
-
+# %% basic raw distributions
+df = pd.read_csv(FINAL_FILENAME)
+sub101 = df.loc[df["Subject"] == 101,]
+sns.displot(sub101, x="RT", col="Cue", row="Prime", hue="Response")
