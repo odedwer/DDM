72f6f82f1358b9a266e38b0ecfd5c6c16c283ce6
odedwer <oded.wertheimer@mail.huji.ac.il>
2021-04-04 19:47:46 +0300

    some minor name changes for columns in the CSV. Start of single subject signal detection theory statistics.

--- generate_csv_from_edat_excel.py
+++ generate_csv_from_edat_excel.py
@@ -3,13 +3,13 @@ import os
 import numpy as np
 
 # %%
-FINAL_FILENAME = 'PVI subjects for ddm.csv'
-FILE_PATH = r"S:\Lab-Shared\Experiments\PVI - Priming Intentions\PVI_EEG_junior_100hz\results\edat"
-EXCLUDED_FILES = [r"PVI_EEGjunior-100hz-behavioral Analysis", r"EMG results","RT_instructed_correct_error"]
-WANTED_COLUMNS = ['Subject', 'Trial', 'PrimeCanvas', 'TargetCanvas', 'ActualResp', 'ResponseTime']
-COLUMN_DICT = {'PrimeCanvas': 'Prime', 'TargetCanvas': 'Cue', 'ResponseTime': "RT", "ActualResp": "Resp"}
-CANVAS_DICT = {1: 'L', 2: 'R', 3: 'FC', 4: 'N'}
-RESPONSE_DICT = {"z": "L", r"{/}": "R", np.nan: "None"}
+FINAL_FILENAME = 'PVI senior subjects for ddm.csv'
+FILE_PATH = r"S:\Lab-Shared\Experiments\PVI - Priming Intentions\PVI_EEG_senior_60hz\results\edat"
+EXCLUDED_FILES = [r"PVI_EEGjunior-100hz-behavioral Analysis", r"EMG results","RT_instructed_correct_error","PVI_EEGsenior_60hz - Behavioral analysis"]
+WANTED_COLUMNS = ['Subject', 'trial', 'PrimeCanvas', 'TargetCanvas', 'ActualResp', 'ResponseTime']
+COLUMN_DICT = {'Subject':'subject','PrimeCanvas': 'prime', 'TargetCanvas': 'cue', 'ResponseTime': "rt", "ActualResp": "resp"}
+CANVAS_DICT = {1: 'l', 2: 'r', 3: 'fc', 4: 'n'}
+RESPONSE_DICT = {"z": "l", r"{/}": "r", np.nan: "none"}
 
 
 def is_results_filename(filename):
@@ -24,12 +24,11 @@ if __name__ == "_main__":
     dataframes = [pd.read_excel(os.path.join(FILE_PATH, filename)) for filename in filenames]
     # concatenate to single dataframe and re-format
     df = pd.concat(dataframes,sort=False)
-    df['Trial'] = df.index + 1
+    df['trial'] = df.index + 1
     df = df[WANTED_COLUMNS]
     df.rename(columns=COLUMN_DICT, inplace=True)
-    df['Resp'].replace(RESPONSE_DICT, inplace=True)
-    df[['Prime', 'Cue']] = df[['Prime', 'Cue']].replace(CANVAS_DICT)
-    df = df[df['RT'].notna()]
-    df = df[df['Subject'].notna()]
-    df['Subject'] = df['Subject'].astype('int')
+    df['resp'].replace(RESPONSE_DICT, inplace=True)
+    df[['prime', 'cue']] = df[['prime', 'cue']].replace(CANVAS_DICT)
+    df = df[df['subject'].notna()]
+    df['subject'] = df['subject'].astype('int')
     df.to_csv(FINAL_FILENAME)
--- main_model.py
+++ main_model.py
@@ -6,6 +6,12 @@ import seaborn as sns
 from generate_csv_from_edat_excel import FINAL_FILENAME
 
 # %% basic raw distributions
-df = pd.read_csv(FINAL_FILENAME)
-sub101 = df.loc[df["Subject"] == 101,]
-sns.displot(sub101, x="RT", col="Cue", row="Prime", hue="Response")
+# RT over
+df = pd.read_csv(FINAL_FILENAME, index_col=0)
+sub1 = df.loc[df["subject"] == 223,]
+sns.displot(sub1, x="rt", col="cue", row="prime", hue="resp", kind="kde",rug=False)
+sns.displot(df, x="rt", col="cue", row="prime", hue="resp", kind="kde")
+# signal detection statistics per subject
+subjectwise_signal_detection = df.groupby('subject').agg(
+    no_resp=pd.NamedAgg(column='resp', aggfunc=lambda col: (col == "none").sum() / col.size)
+)
