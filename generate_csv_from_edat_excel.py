a15e6ceada8b863403753ca40b9d966320d412ef
odedwer <oded.wertheimer@mail.huji.ac.il>
2021-04-04 18:48:40 +0300

    changed generation of concatenated CSV of behavioral results to be from actual experiment, not pilot.

--- generate_csv_from_edat_excel.py
+++ generate_csv_from_edat_excel.py
@@ -4,20 +4,26 @@ import numpy as np
 
 # %%
 FINAL_FILENAME = 'PVI subjects for ddm.csv'
-FILE_PATH = r"\\ems.elsc.huji.ac.il\deouell-lab\Lab-Shared\Experiments\PVI - Priming Intentions\PVI\results"
+FILE_PATH = r"S:\Lab-Shared\Experiments\PVI - Priming Intentions\PVI_EEG_junior_100hz\results\edat"
+EXCLUDED_FILES = [r"PVI_EEGjunior-100hz-behavioral Analysis", r"EMG results","RT_instructed_correct_error"]
 WANTED_COLUMNS = ['Subject', 'Trial', 'PrimeCanvas', 'TargetCanvas', 'ActualResp', 'ResponseTime']
 COLUMN_DICT = {'PrimeCanvas': 'Prime', 'TargetCanvas': 'Cue', 'ResponseTime': "RT", "ActualResp": "Resp"}
 CANVAS_DICT = {1: 'L', 2: 'R', 3: 'FC', 4: 'N'}
 RESPONSE_DICT = {"z": "L", r"{/}": "R", np.nan: "None"}
 
+
+def is_results_filename(filename):
+    return (filename[-4:] == 'xlsx' or filename[-3:] == 'xls') and (filename[:-5] not in EXCLUDED_FILES)
+
+
 if __name__ == "_main__":
     # load filenames
     filenames = [filename for filename in os.listdir(FILE_PATH) if
-                 filename[:3].lower() == 'pvi' and filename[-4:] == 'xlsx' and 'digest' not in filename]
+                 is_results_filename(filename) and (filename[:-5] not in EXCLUDED_FILES)]
     # read as pandas data frames
     dataframes = [pd.read_excel(os.path.join(FILE_PATH, filename)) for filename in filenames]
     # concatenate to single dataframe and re-format
-    df = pd.concat(dataframes)
+    df = pd.concat(dataframes,sort=False)
     df['Trial'] = df.index + 1
     df = df[WANTED_COLUMNS]
     df.rename(columns=COLUMN_DICT, inplace=True)
